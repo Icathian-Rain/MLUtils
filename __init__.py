5ae87ba58dd8dd7b58c43b7593c7b7a1561a59a3
IcathianRain <2205794866@qq.com>
2024-04-19 16:16:47 +0800

    ✨ feat(logs): add logs class
    Add Logs class to logs.py and update __init__.py to include Logs in __all__

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from .logs import getLogger, getWriter
+from .logs import getLogger, getWriter, Logs
 
 
 def getName(prefix: str):
     return prefix + datetime.now().strftime("%Y%m%d-%H%M%S")
 
-__all__ = ["getName", getLogger, getWriter]
+__all__ = ["getName", getLogger, getWriter, Logs]
--- logs.py
+++ logs.py
@@ -1,6 +1,7 @@
 import os
 from torch.utils.tensorboard import SummaryWriter
 import logging
+from typing import Union
 
 def getWriter(name):
     return SummaryWriter(log_dir=os.path.join("logs", name))
@@ -22,6 +23,26 @@ def getLogger(name, level=logging.INFO, loginFile=True):
         logger.addHandler(fh)
     return logger
 
+class Logs:
+    def __init__(self, name, level=logging.INFO, loginFile=True):
+        self.logger = getLogger(name, level, loginFile)
+        self.writer = getWriter(name)
+    
+    # 添加数据到tensorboard
+    def add_scalar(self, tag : Union[str, list], scalar_values: Union[int, float, list], global_step=None, walltime=None):
+        if isinstance(scalar_values, (int, float)) and isinstance(tag, str):
+            self.writer.add_scalar(tag, scalar_values, global_step, walltime)
+            self.logger.info(f'global_step: {global_step}, {tag}: {scalar_values}')
+        elif isinstance(scalar_values, list) and isinstance(tag, list) and len(scalar_values) == len(tag):
+            msg = f'global_step: {global_step}, '
+            for i, t in enumerate(tag):
+                self.writer.add_scalar(t, scalar_values[i], global_step, walltime)
+                msg += f'{t}: {scalar_values[i]}, '
+            self.logger.info(msg)
+        else:
+            raise ValueError("tag and scalar_values should be the same type")
+        
+
 if __name__ == "__main__":
     logger = getLogger("test")
     logger.info("test")
@@ -31,3 +52,7 @@ if __name__ == "__main__":
     writer = getWriter("test")
     writer.add_text("test", "test")
     writer.close()
+    logs = Logs("test1")
+    logs.add_scalar("val", 1, 1)
+    logs.add_scalar(["val1", "val2"], [1, 2], 2)
+    logs.add_scalar("val2", [1, 2], 1)
