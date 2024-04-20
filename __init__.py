35ae401c730219ffb7aeb4d5c93355a99855312d
IcathianRain <2205794866@qq.com>
2024-04-20 10:19:07 +0800

    ✨ feat(acc): add AverageMeter
    Add AverageMeter class to acc.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from .logs import getLogger, getWriter, Logs
+from .acc import AverageMeter
 
 
 def getName(prefix: str):
     return prefix + datetime.now().strftime("%Y%m%d-%H%M%S")
 
-__all__ = ["getName", getLogger, getWriter, Logs]
+__all__ = ["getName", getLogger, getWriter, Logs, AverageMeter]
